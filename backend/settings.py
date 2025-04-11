245ee384586cbff88beda91ce5003477beb98cbe
Aashi-1310 <117965591+Aashi-1310@users.noreply.github.com>
2025-04-11 15:25:01 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,9 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = (BASE_DIR, 'staticfiles')  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collectstatic will store final static files
+
 STATICFILES_DIRS = [
-    (BASE_DIR, 'static'),  
+    os.path.join(BASE_DIR, 'static'),  # Where your custom static files are located
 ]
 
 
